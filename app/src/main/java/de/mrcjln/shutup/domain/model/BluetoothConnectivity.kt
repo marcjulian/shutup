882678c851c16892d267dce98751a3374e5ecda2
Marc Stammerjohann <marcstammerjohann@web.de>
2017-12-15 23:27:48 +0100

    shut up connectivity only if required
    * choose to shut up wifi or bluetooth through ui
    * only shut up and turn on connectivity if it was enabled before

--- app/src/main/java/de/mrcjln/shutup/domain/model/BluetoothConnectivity.kt
+++ app/src/main/java/de/mrcjln/shutup/domain/model/BluetoothConnectivity.kt
@@ -3,6 +3,7 @@ package de.mrcjln.shutup.domain.model
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import org.cryptomator.util.SharedPreferencesHandler
 import timber.log.Timber
 
 class BluetoothConnectivity(context: Context) : Connectivity {
@@ -10,14 +11,22 @@ class BluetoothConnectivity(context: Context) : Connectivity {
     private val bluetoothAdapter: BluetoothAdapter
             = (context.applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
 
+    private val sharedPreferenceHandler: SharedPreferencesHandler
+            = SharedPreferencesHandler(context)
+
     override fun turnOn() {
-        Timber.tag("BluetoothConnectivity").d("turn on")
-        bluetoothAdapter.enable()
+        if (sharedPreferenceHandler.shutUpBluetooth() && sharedPreferenceHandler.wasBluetoothOn()) {
+            Timber.tag("BluetoothConnectivity").d("turn on")
+            bluetoothAdapter.enable()
+        }
     }
 
     override fun turnOff() {
-        Timber.tag("BluetoothConnectivity").d("turn off")
-        bluetoothAdapter.disable()
+        sharedPreferenceHandler.setBluetoothWasOn(isOn())
+        if (sharedPreferenceHandler.shutUpBluetooth() && isOn()) {
+            Timber.tag("BluetoothConnectivity").d("turn off")
+            bluetoothAdapter.disable()
+        }
     }
 
     override fun isOn(): Boolean = bluetoothAdapter.isEnabled
--- app/src/main/java/de/mrcjln/shutup/presentation/ui/ShutUpActivity.kt
+++ app/src/main/java/de/mrcjln/shutup/presentation/ui/ShutUpActivity.kt
@@ -1,13 +1,50 @@
 package de.mrcjln.shutup.presentation.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import de.mrcjln.shutup.R
+import kotlinx.android.synthetic.main.activity_shut_up.*
+import org.cryptomator.util.SharedPreferencesHandler
 
 class ShutUpActivity : AppCompatActivity() {
+
+    private var sharedPreferencesHandler: SharedPreferencesHandler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shut_up)
+        sharedPreferencesHandler = SharedPreferencesHandler(this)
+        setupView()
     }
+
+    private fun setupView() {
+        updateWifiConnectivity()
+        updateBluetoothConnectivity()
+
+        wifiConnectivity.setOnClickListener {
+            sharedPreferencesHandler!!.revertShutUpWifi()
+            updateWifiConnectivity()
+        }
+        bluetoothConnectivity.setOnClickListener {
+            sharedPreferencesHandler!!.revertShutUpBluetooth()
+            updateBluetoothConnectivity()
+        }
+    }
+
+    private fun updateWifiConnectivity() {
+        if (sharedPreferencesHandler!!.shutUpWifi()) {
+            wifiConnectivity.setImageResource(R.drawable.ic_wifi_off)
+        } else {
+            wifiConnectivity.setImageResource(R.drawable.ic_wifi)
+        }
+    }
+
+    private fun updateBluetoothConnectivity() {
+        if (sharedPreferencesHandler!!.shutUpBluetooth()) {
+            bluetoothConnectivity.setImageResource(R.drawable.ic_bluetooth_disabled)
+        } else {
+            bluetoothConnectivity.setImageResource(R.drawable.ic_bluetooth)
+        }
+    }
+
 }
